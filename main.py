970265aba8979d065561e0356a6eb11e21b9e080
Sandra Jeje <B00145528@mytudublin.ie>
2026-04-28 17:05:54 +0100

    my second commit
    added names and service

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ class HairBookingApp:
         self.main_window.title("Hair Booking App")
         self.main_window.geometry("500x400")
 
+
+        self.name_var = StringVar()
+        self.service_var = StringVar()
+
+        Label(self.main_window, text="Name").pack()
+        Entry(self.main_window, textvariable=self.name_var).pack()
+
+        Label(self.main_window, text="Service").pack()
+        self.service_menu = OptionMenu(self.main_window, self.service_var, "Haircut", "Colour", "Blow Dry")
+        self.service_menu.pack()
+
+
         self.listbox = Listbox(self.main_window, width=50)
         self.listbox.pack(pady=20)
 
@@ -23,3 +35,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
