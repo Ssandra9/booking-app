f739f4247c692791f2e9ffa1f6c0a4e7d7639e38
Sandra Jeje <B00145528@mytudublin.ie>
2026-04-28 15:40:43 +0100

    my first commit
    adding file im working on

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import tinker as tk
+
+
+class BookingApp:
+    def __init__(self):
+      self.main_window = Tk()
+      self.main_window.geometry("500x400")
+      self.main_window.title("Booking System")
+
